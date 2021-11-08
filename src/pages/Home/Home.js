b5f7731f4be0db06ade440c83f3099bc522f188c
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-11-08 14:55:25 -0300

    Pastas pages e components

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import './home.scss'
+
+function Home() {
+    return (
+      <div className="header">
+          <img src="" alt=""/>
+          <div>
+            <h1> Olá, Maria!</h1>
+            <span>⭐⭐⭐⭐⭐</span>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
