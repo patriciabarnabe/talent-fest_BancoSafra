3c2a0972d4e7d4e7a5e0539d320e0f3992ed667b
jenniferpessoa <jenni.pessoa@gmail.com>
2021-11-09 15:29:38 -0300

    navbar com links funcionando

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,1 +1,19 @@
-import Button from 'react-bootstrap/Button'
+import React from 'react';
+import { Link } from "react-router-dom";
+
+import "./navbar.css"
+
+const Navbar = () => {
+
+  return (
+    <nav className="navbar">
+      <Link className="link" to="/home" ><i className="fas fa-home" /></Link>
+      <Link className="link" to="/article" ><i className="fas fa-book-open" /></Link>
+
+      <Link className="link" to="/saves"><i className="far fa-bookmark" /></Link>
+      <Link className="link" to="/profile"><i className="far fa-user" /></Link>
+    </nav>
+  );
+}
+
+export default Navbar;
