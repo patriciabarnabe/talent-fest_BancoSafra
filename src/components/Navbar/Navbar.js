801b7cb46e8acaab3881b07bb2da4957adab0455
jenniferpessoa <jenni.pessoa@gmail.com>
2021-11-09 15:31:42 -0300

    conflitos resolvidos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'rect-router-dom';
 
-import "./navbar.css"
+import './navbar.css';
 
 const Navbar = () => {
 
