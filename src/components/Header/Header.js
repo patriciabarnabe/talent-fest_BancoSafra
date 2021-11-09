c794e530349736d0d21a2b5468b94e779279ea7d
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-11-09 18:37:32 -0300

    Iniciando componente Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './../../assets/logo-branca.png'
+
+import './header.css';
+
+const Header = () => {
+
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img src={logo} alt='Logo Banco Safra' />
+      </Link>  
+      <p> Menina de Vila </p>
+    </header>
+  );
+}
+
+export default Header;
