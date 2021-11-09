9b35211f163f2a7fb60f16870afb98fd516b5284
jenniferpessoa <jenni.pessoa@gmail.com>
2021-11-09 15:38:37 -0300

    conflitos resolvidos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from 'rect-router-dom';
+import { Link } from 'react-router-dom';
 
 import './navbar.css';
 
 const Navbar = () => {
 
   return (
-    <nav className="navbar">
-      <Link className="link" to="/home" ><i className="fas fa-home" /></Link>
-      <Link className="link" to="/article" ><i className="fas fa-book-open" /></Link>
+    <nav className='navbar'>
+      <Link className='link' to='/home' ><i className='fas fa-home' /></Link>
+      <Link className='link' to='/article' ><i className='fas fa-book-open' /></Link>
 
-      <Link className="link" to="/saves"><i className="far fa-bookmark" /></Link>
-      <Link className="link" to="/profile"><i className="far fa-user" /></Link>
+      <Link className='link' to='/saves'><i className='far fa-bookmark' /></Link>
+      <Link className='link' to='/profile'><i className='far fa-user' /></Link>
     </nav>
   );
 }
