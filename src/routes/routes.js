b916c018047a9ed0268253b0374161573b2bd2bd
Joice Souza <joice_fsouza@hotmail.com>
2021-11-08 21:56:54 -0300

    pasta Login e Modal

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,12 +5,14 @@ import {
 
 import Home from '../pages/Home/Home'
 import PageNotFound from '../pages/Notfound/Notfound'
+import Login from "../pages/Login/Login";
 
 export const Rotas = () => {
   return (
     <Routes>
       <Route path='/home' element={<Home />} />
       <Route path='*' element={<PageNotFound />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   )
 }
