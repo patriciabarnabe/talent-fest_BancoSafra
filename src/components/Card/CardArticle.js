d0d86b79c79a2e83a78c3fa23c05b2d74efaabfc
Carmen Emilia Gozza <carmemilya1@gmail.com>
2021-11-10 16:20:34 -0300

    pagina article ok, falta stilização

--- src/components/Card/CardArticle.js
+++ src/components/Card/CardArticle.js
@@ -2,50 +2,20 @@ import React from "react";
 
 import './card.css'
 
-const CardArticle = (article) => {
+const CardArticle = ({ article, children }) => {
 
     return (
         <article className='article'>
             <div className="container-card">
+
                 <div className="card">
                     <div className="face face1">
                         <div className="content">
                             <span className="stars"></span>
-                            { article && article.map((trilha, index) => {
-                
-                                return (
-                                    <div key={index}>
-                                        <div>
-                                            <div>{trilha.titulo}</div>
-                                            <div>{trilha.autora}</div>
-                                            <section>
-                                                {trilha.conteudo[0]}
-                                            </section>
-                                            <section>
-                                                {trilha.conteudo[1]}
-                                                
-                                            </section>
-                                        </div>
-                                        <div>{article.likes}</div>
-                                    </div> 
-                                )
-                            })}
+                            <p className="text-card">{article}</p>
+                            {children}
                         </div>
                     </div>
-                    {/* <div className="face face2">
-                        <section className='info-card'>
-                            <h2 className='title-card'>{article.titulo}</h2>
-                            <div className='icons-card'>
-                                <i className="far fa-bookmark"></i>
-                                <i className="far fa-heart"><span className="number-likes">{article.likes}</span></i>
-                            </div>
-                        </section>
-                        <img className='image-card'
-                            src={article.imagem}
-                        />
-                        <h3 className='category-card'>{article.categoria}</h3>
-
-                    </div> */}
                 </div>
             </div>
         </article>
