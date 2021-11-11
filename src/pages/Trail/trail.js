a3a1b7a10fc224496bfc0084b09fefe2b507af27
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-11-11 12:21:33 -0300

    fixing pull

--- src/pages/Trail/trail.js
+++ src/pages/Trail/trail.js
@@ -1,39 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { collection, getDocs, } from '@firebase/firestore';
-import { db } from '../../services/firebase';
 
-export default function Trail() { 
-  let { id } = useParams()
-  // let idParam = Number(JSON.parse(JSON.stringify(useParams())).id);
+function Trail() {
+  function sim() {
+    let { id } = useParams()
+  console.log(id) 
+  return id
+  }
 
-  const [trilha, setTrilha] = useState([])
 
-useEffect(() => {
-  const trilhas = collection(db, 'trilha');
-  const esperandoGetDocs = getDocs(trilhas)
-  esperandoGetDocs.then((collectionTrilha) => {
-       const newArray = [];
-      collectionTrilha.forEach((doc) => {
-
-          console.log(doc.data()); 
-           
-          newArray.push(doc.data());
-      });
-      console.log(newArray);
-      setTrilha(newArray);
-
-  })
-}, [])
-
-  console.log();
   return (
-    <div>{trilha[id]?.conteudo.map((content) => {
-      console.log(content);
-    })}</div>
+    <div>{() => sim()}</div>
   );
   
 };
 
-
-
+export default Trail;
