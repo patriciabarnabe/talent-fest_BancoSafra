83a038435801b4081dc31f64ff233d0b021603bb
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-11-12 10:42:23 -0300

    Ajuste deploy

--- src/pages/Trail/trail.js
+++ src/pages/Trail/trail.js
@@ -1,31 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { collection, getDocs, } from '@firebase/firestore';
-import { db } from '../../services/firebase';
 
-export default function Trail() { 
-  let { id } = useParams()
+function Trail() {
+  function sim() {
+    let { id } = useParams()
+  console.log(id) 
+  return id
+  }
 
-  const [trilha, setTrilha] = useState([])
-
-useEffect(() => {
-  const trilhas = collection(db, 'trilha');
-  const esperandoGetDocs = getDocs(trilhas)
-  esperandoGetDocs.then((collectionTrilha) => {
-       const newArray = [];
-      collectionTrilha.forEach((doc) => {
-
-           
-          newArray.push(doc.data());
-      });
-      setTrilha(newArray);
-
-  })
-}, [])
 
   return (
-    <div>{trilha[id]?.conteudo.map((content) => {
-    })}</div>
+    <div>{() => sim()}</div>
   );
   
 };
+
+export default Trail;
